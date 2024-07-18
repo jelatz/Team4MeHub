08d63972fc95beb3dba5b660e2d7512618bafaa8
ctc_lad <ctc_lad@mail2.calltekcenter.com>
2024-07-18 16:57:28 +0800

    additional

--- application/views/users/profile.php
+++ application/views/users/profile.php
@@ -12,7 +12,7 @@
     </div>
     <div class="row px-3">
         <div class="col-md-3">
-            <div class="card border-4 border-start-0 border-end-0 border-bottom-0 shadow border-warning">
+            <div class="card border-4 border-start-0 border-end-0 border-bottom-0 shadow-sm border-warning">
                 <div class="card-body border-0 bg-light-subtle p-3 position-relative">
                     <img src="http://localhost/team4mehub/assets/dist/img/avatar.png" alt="User Image" class="img-fluid rounded-circle mx-auto d-block border border-3 p-1" width="100">
                     <p class="mt-2 text-center">
@@ -32,15 +32,17 @@
         </div>
 
         <div class="col-md-9 shadow-sm p-0">
-            <nav class="nav nav-tabs" id="nav-tab" role="tablist">
-                <a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-                <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-                <a class="nav-link disabled" id="nav-disabled-tab" data-bs-toggle="tab" href="#nav-disabled" role="tab" aria-controls="nav-disabled" tabindex="-1" aria-disabled="true">Disabled</a>
-            </nav>
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore tempora molestiae pariatur, voluptate fuga corrupti est reiciendis maxime totam dolores, voluptates, dolorem eaque sequi.</div>
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, natus sed soluta necessitatibus tempore aspernatur? Praesentium, odit explicabo distinctio dolore adipisci officia iure, ut magnam optio aliquam at similique veritatis.</div>
-                <div class="tab-pane fade" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium minima repellat incidunt facilis obcaecati blanditiis corrupti ad officia doloribus ullam sapiente ipsum, nemo a, excepturi voluptatem voluptatibus velit eum dignissimos ut, nam tempora? Reiciendis illo itaque veritatis eligendi fuga, mollitia ratione totam veniam esse in.</div>
+            <ul class="nav nav-tabs" id="profileNavTab" role="tablist">
+                <li class="<?= ($active == "details") ? "active" : "" ?>"><a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#basicInfo" role="tab" aria-controls="nav-home" aria-selected="true">Basic Info</a></li>
+                <li class="<?= ($active == "training") ? "active" : "" ?>"> <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#training" role="tab" aria-controls="nav-profile" aria-selected="false">Training/Skills/Awards</a></li>
+                <li class="<?= ($active == "changepass") ? "active" : "" ?>"> <a class="nav-link" id="changePass" data-bs-toggle="tab" href="#changePass" role="tab" aria-controls="changePass" aria-selected="true">Change Password</a></li>
+            </ul>
+            <div class="tab-content" id="profileTabContent">
+                <div class="tab-pane fade show active" id="basicInfo" role="tabpanel" aria-labelledby="nav-basicInfo-tab">
+                    <?php include APPPATH . '/views/components/Basic_info.php'; ?>
+                </div>
+                <div class="tab-pane fade" id="training" role="tabpanel" aria-labelledby="nav-training-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, natus sed soluta necessitatibus tempore aspernatur? Praesentium, odit explicabo distinctio dolore adipisci officia iure, ut magnam optio aliquam at similique veritatis.</div>
+                <div class="tab-pane fade" ids="changePass" role="tabpanel" aria-labelledby="nav-changePass-tab">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium minima repellat incidunt facilis obcaecati blanditiis corrupti ad officia doloribus ullam sapiente ipsum, nemo a, excepturi voluptatem voluptatibus velit eum dignissimos ut, nam tempora? Reiciendis illo itaque veritatis eligendi fuga, mollitia ratione totam veniam esse in.</div>
             </div>
         </div>
     </div>
